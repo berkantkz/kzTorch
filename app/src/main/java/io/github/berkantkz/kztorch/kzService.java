79496fd1e8d1ed00adbddaf8f4ffc410c7c80842
berkantkz <berkantk3@gmail.com>
2018-08-28 19:11:56 +0300

    Add new features
    * Notification with a turn off action when it's on
    * Changed tile icon to a suitable one with the app icon

--- app/src/main/java/io/github/berkantkz/kztorch/kzService.java
+++ app/src/main/java/io/github/berkantkz/kztorch/kzService.java
@@ -1,12 +1,16 @@
 package io.github.berkantkz.kztorch;
 
-import android.app.ProgressDialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Icon;
-import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -19,69 +23,86 @@ import eu.chainfire.libsuperuser.Shell;
 
 public class kzService extends TileService {
 
-    String torch_path = "/sys/class/leds/led:torch_1/brightness";
+    static String torch_path = "/sys/class/leds/led:torch_1/brightness";
     int torch_level, torch_status;
-    boolean torch_current_status, isUserFriendly;
+    static boolean torch_current_status;
     SharedPreferences sharedPreferences;
+    static NotificationManagerCompat notificationManager;
+    static NotificationCompat.Builder mBuilder;
 
     @Override
     public void onClick() {
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        Shell.SH.run("echo 1 > " + torch_path);
-        if (torch_status != 0) {
-            Toast.makeText(kzService.this, "You have a user-friendly boot image!", Toast.LENGTH_SHORT).show();
-            isUserFriendly = true;
-            Shell.SH.run("echo 0 > " + torch_path);
-        } else {
-            Toast.makeText(kzService.this, "No way! You must have root access.", Toast.LENGTH_SHORT).show();
-            isUserFriendly = false;
-            Shell.SH.run("echo 0 > " + torch_path);
-        }
-
         if (!sharedPreferences.getBoolean("pref_disable_ads",true)) {
             MainActivity.startinterstitialAd(kzService.this);
         }
 
-        if (isUserFriendly) {
+        if (Shell.SU.available()) {
             if (sharedPreferences.contains("pref_torch_level")) {
                 torch_level = sharedPreferences.getInt("pref_torch_level", 0);
             } else {
                 torch_level = 255;
                 sharedPreferences.edit().putInt("pref_torch_level", torch_level).apply();
             }
-        } else {
-            if (Shell.SU.available()) {
-                if (sharedPreferences.contains("pref_torch_level")) {
-                    torch_level = sharedPreferences.getInt("pref_torch_level", 0);
-                } else {
-                    torch_level = 255;
-                    sharedPreferences.edit().putInt("pref_torch_level", torch_level).apply();
-                }
 
-                // Check whether the torch is on or not for first run.
-                torch_status = Integer.parseInt(String.valueOf(Shell.SU.run(new String[]{"cat " + torch_path})).replace("[", "").replace("]", ""));
+            // Check whether the torch is on or not for first run.
+            torch_status = Integer.parseInt(String.valueOf(Shell.SU.run(new String[]{"cat " + torch_path})).replace("[", "").replace("]", ""));
 
-                if (torch_status == 0) {
-                    torch_current_status = true;
-                    Shell.SU.run("echo " + torch_level + "> " + torch_path);
-                    updateTile(R.drawable.twotone_flash_on_white_24dp);
-                    Log.d("berkantkz", "kz Torch turned on");
-                } else {
-                    torch_current_status = false;
-                    Shell.SU.run("echo 0 > " + torch_path);
-                    updateTile(R.drawable.twotone_flash_off_white_24dp);
-                    Log.d("berkantkz", "kz Torch turned off");
-                }
+            // Intent for notification action
+            Intent turnOffIntent = new Intent(this, kzService.ActionReceiver.class);
+            PendingIntent turnOffPendingIntent = PendingIntent.getBroadcast(this, 1, turnOffIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+            mBuilder = new NotificationCompat.Builder(this, "kzTorch")
+                    .setContentTitle(getString(R.string.notification_on_title))
+                    .setContentText(getString(R.string.notification_on_content))
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(getString(R.string.notification_on_content)))
+                    .setSmallIcon(R.drawable.ic_tile)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true)
+                    .setOngoing(true)
+                    .addAction(R.drawable.twotone_flash_off_white_24dp, getString(R.string.notification_action_turn_off), turnOffPendingIntent);
+            notificationManager = NotificationManagerCompat.from(this);
+
+            if (torch_status == 0) {
+                turnOn();
             } else {
-                Toast.makeText(this, R.string.root_status_false, Toast.LENGTH_SHORT).show();
+                turnOff();
             }
+        } else {
+            Toast.makeText(this, R.string.root_status_false, Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void updateTile(int icon) {
+    public void turnOn() {
+        torch_current_status = true;
+        Shell.SU.run("echo " + torch_level + "> " + torch_path);
+        Log.d("berkantkz", "kz Torch turned on");
+        notificationManager.notify(1, mBuilder.build());
+    }
+
+    public void turnOff() {
+        torch_current_status = false;
+        Shell.SU.run("echo 0 > " + torch_path);
+        Log.d("berkantkz", "kz Torch turned off");
+        notificationManager.cancel(1);
+    }
+
+    private void updateTile(int icon, String label) {
         final Tile tile = getQsTile();
         tile.setIcon(Icon.createWithResource(getApplicationContext(), icon));
+        tile.setLabel(label);
         tile.updateTile();
+    }
+
+    public static class ActionReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            notificationManager.cancel(1);
+            torch_current_status = false;
+            Shell.SU.run("echo 0 > " + torch_path);
+            Log.d("berkantkz", "kz Torch turned off");
+            notificationManager.cancel(1);
+        }
     }
 }
