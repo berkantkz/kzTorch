ec87a4ccf23581a7176c60ea5b7d0904db8e07d6
berkantkz <berkantk3@gmail.com>
2018-08-29 03:56:57 +0300

    Add actions to incerease/decrease the level when it's on

--- app/src/main/java/io/github/berkantkz/kztorch/kzService.java
+++ app/src/main/java/io/github/berkantkz/kztorch/kzService.java
@@ -23,8 +23,9 @@ import eu.chainfire.libsuperuser.Shell;
 
 public class kzService extends TileService {
 
-    static String torch_path = "/sys/class/leds/led:torch_1/brightness";
-    int torch_level, torch_status;
+    //static String torch_path = "/sys/class/leds/flashlight-front/brightness";
+    static String torch_path = "/sys/class/leds/led:torch_1/brightness", appTag = "kzTorch";
+    static int torch_level, torch_status;
     static boolean torch_current_status;
     SharedPreferences sharedPreferences;
     static NotificationManagerCompat notificationManager;
@@ -49,9 +50,20 @@ public class kzService extends TileService {
             // Check whether the torch is on or not for first run.
             torch_status = Integer.parseInt(String.valueOf(Shell.SU.run(new String[]{"cat " + torch_path})).replace("[", "").replace("]", ""));
 
-            // Intent for notification action
+            // Intent for turning off the torch
             Intent turnOffIntent = new Intent(this, kzService.ActionReceiver.class);
+            turnOffIntent.setAction("turnoff");
             PendingIntent turnOffPendingIntent = PendingIntent.getBroadcast(this, 1, turnOffIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+            // Intent for increasing the level of the torch
+            Intent increaseIntent = new Intent(this, kzService.ActionReceiver.class);
+            increaseIntent.setAction("increase");
+            PendingIntent increasePendingIntent = PendingIntent.getBroadcast(this, 1, increaseIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+            // Intent for decreasing the level of the torch
+            Intent decreaseIntent = new Intent(this, kzService.ActionReceiver.class);
+            decreaseIntent.setAction("decrease");
+            PendingIntent decreasePendingIntent = PendingIntent.getBroadcast(this, 1, decreaseIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
             mBuilder = new NotificationCompat.Builder(this, "kzTorch")
                     .setContentTitle(getString(R.string.notification_on_title))
@@ -61,6 +73,8 @@ public class kzService extends TileService {
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                     .setAutoCancel(true)
                     .setOngoing(true)
+                    .addAction(R.drawable.ic_tile, getString(R.string.increase), increasePendingIntent)
+                    .addAction(R.drawable.ic_tile, getString(R.string.decrease), decreasePendingIntent)
                     .addAction(R.drawable.ic_tile, getString(R.string.notification_action_turn_off), turnOffPendingIntent);
             notificationManager = NotificationManagerCompat.from(this);
 
@@ -77,15 +91,50 @@ public class kzService extends TileService {
     public void turnOn() {
         torch_current_status = true;
         Shell.SU.run("echo " + torch_level + "> " + torch_path);
-        Log.d("berkantkz", "kz Torch turned on");
+        Log.d(appTag, "Turned on\nLevel=" + torch_level);
         notificationManager.notify(1, mBuilder.build());
     }
 
     public void turnOff() {
         torch_current_status = false;
         Shell.SU.run("echo 0 > " + torch_path);
-        Log.d("berkantkz", "kz Torch turned off");
+        Log.d(appTag, "kz Torch turned off");
         notificationManager.cancel(1);
+    }
+
+    public static void increase() {
+        if (torch_level < 255) {
+            int increasedLevel;
+            if (torch_level >= 200) {
+                increasedLevel = 255;
+            } else {
+                increasedLevel = torch_level + 55;
+            }
+            torch_level = increasedLevel;
+            Shell.SU.run("echo " + torch_level + "> " + torch_path);
+            Log.d(appTag, "Value increased\nLevel = " + String.valueOf(increasedLevel));
+        } else {
+            Log.w(appTag,"Highest value reached.");
+        }
+    }
+
+    public static void decrease() {
+        if (torch_level <= 255) {
+            int decreasedLevel;
+            if (torch_level <= 55) {
+                decreasedLevel = 0;
+                notificationManager.cancel(1);
+                torch_current_status = false;
+                Shell.SU.run("echo 0 > " + torch_path);
+                Log.d(appTag, "Turned off");
+                notificationManager.cancel(1);
+            } else {
+                decreasedLevel = torch_level - 55;
+            }
+            torch_level = decreasedLevel;
+            Shell.SU.run("echo " + torch_level + "> " + torch_path);
+            Log.d(appTag, "Value decreased\nLevel = " + String.valueOf(decreasedLevel));
+        }
     }
 
     private void updateTile(int icon, String label) {
@@ -98,11 +147,22 @@ public class kzService extends TileService {
     public static class ActionReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            notificationManager.cancel(1);
-            torch_current_status = false;
-            Shell.SU.run("echo 0 > " + torch_path);
-            Log.d("berkantkz", "kz Torch turned off");
-            notificationManager.cancel(1);
+            String type = intent.getAction();
+            switch (String.valueOf(type)) {
+                case "turnoff":
+                    notificationManager.cancel(1);
+                    torch_current_status = false;
+                    Shell.SU.run("echo 0 > " + torch_path);
+                    Log.d(appTag, "Turned off");
+                    notificationManager.cancel(1);
+                    break;
+                case "increase":
+                    increase();
+                    break;
+                case "decrease":
+                    decrease();
+                    break;
+            }
         }
     }
 }
